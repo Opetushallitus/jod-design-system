46ee3a4eb416fa317ec015ae7ede92de34381fed
Sami Korpela <sami.korpela@gofore.com>
2024-03-08 16:04:11 +0200

    OPHJOD-241: Update files to be line with prettier/eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import dts from "vite-plugin-dts";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({ include: ["lib"], exclude: ["lib/**/*.stories.{ts,tsx}"] }),
-  ],
+  plugins: [react(), dts({ include: ['lib'], exclude: ['lib/**/*.stories.{ts,tsx}'] })],
   build: {
     copyPublicDir: false,
     lib: {
-      fileName: "main",
-      entry: resolve(__dirname, "lib/main.ts"),
-      formats: ["es"],
+      fileName: 'main',
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es'],
     },
     rollupOptions: {
-      external: ["react", "react/jsx-runtime"],
+      external: ['react', 'react/jsx-runtime'],
     },
   },
 });
