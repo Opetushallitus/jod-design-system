cbc5e155daff4947eb2e025ab39aa01a4a15db27
Sami Korpela <sami.korpela@gofore.com>
2024-03-08 16:03:58 +0200

    OPHJOD-241: Add prettier and eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,31 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:storybook/recommended',
+    'plugin:sonarjs/recommended',
+    'prettier', // must be last, override other configs
+  ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parserOptions: {
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
   },
-}
+  settings: {
+    react: { version: 'detect' },
+  },
+  plugins: ['react-refresh', 'jsx-a11y', 'sonarjs'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/jsx-child-element-spacing': 'error',
+    'no-useless-rename': 'error',
+  },
+};
