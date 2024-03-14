2fac20a531f5ed80da148323e60cffbc6085d29c
Sami Korpela <sami.korpela@gofore.com>
2024-03-14 09:05:09 +0200

    OPHJOD-263: Fix CSS import for bundled JS

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ['lib'], exclude: ['lib/**/*.stories.{ts,tsx}'] })],
+  plugins: [react(), libInjectCss(), dts({ include: ['lib'], exclude: ['lib/**/*.stories.{ts,tsx}'] })],
   test: {
     environment: 'jsdom',
   },
