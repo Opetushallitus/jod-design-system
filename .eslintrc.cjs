5ff099183d27a2fb76c0d9868447f566637a5cba
Sauli Anto <sauli.anto@gofore.com>
2024-03-19 17:10:15 +0200

    OPHJOD-274: Fix Storybook linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
     'plugin:sonarjs/recommended',
     'prettier', // must be last, override other configs
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'storybook-static'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'storybook-static', '!.storybook'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: ['./tsconfig.json', './tsconfig.node.json'],
