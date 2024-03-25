3d8e9c1a7d0489cdd160548884dd9b7444a28e99
Sauli Anto <sauli.anto@gofore.com>
2024-03-25 10:49:55 +0200

    OPHJOD-280: Add JOD theme and addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,32 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../lib/**/*.mdx', '../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    '../lib/**/*.mdx',
+    {
+      directory: '../lib/components',
+      files: '**/*.stories.@(js|jsx|mjs|ts|tsx)',
+      titlePrefix: 'Components',
+    },
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-themes',
     '@storybook/addon-designs',
   ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
+  core: {
+    disableTelemetry: true,
+  },
   docs: {
     autodocs: 'tag',
+    defaultName: 'Documentation',
   },
 };
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import dts from 'vite-plugin-dts';
-import { resolve } from 'path';
+import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
