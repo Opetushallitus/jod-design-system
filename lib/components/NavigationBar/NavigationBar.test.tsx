89f17adc87c51a80c5c5185b5f89cb5152086469
Sauli Anto <sauli.anto@gofore.com>
2024-03-25 09:22:04 +0200

    OPHJOD-276: Add navigation bar

---
+++ lib/components/NavigationBar/NavigationBar.test.tsx
@@ -0,0 +1,120 @@
+import { afterEach, describe, it, expect } from 'vitest';
+import { render, screen, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import { NavigationBar, NavigationBarLinkProps } from './NavigationBar';
+
+afterEach(() => {
+  cleanup();
+});
+
+describe('NavigationBar', () => {
+  const items = [
+    {
+      text: 'Home',
+      active: true,
+      href: '/home',
+    },
+    {
+      text: 'About',
+      active: false,
+      href: '/about',
+    },
+    {
+      text: 'Contact',
+      active: false,
+      href: '/contact',
+    },
+  ].map(({ text, active, href }) => ({
+    text,
+    active,
+    href,
+    component: ({ children, ...rootProps }: NavigationBarLinkProps) => (
+      <a href={href} {...rootProps}>
+        {children}
+      </a>
+    ),
+  }));
+
+  const user = {
+    name: 'Jane Doe',
+    src: 'user.jpg',
+    component: ({ children, ...rootProps }: NavigationBarLinkProps) => (
+      <a href="/profile" aria-label="Profile" {...rootProps}>
+        {children}
+      </a>
+    ),
+  };
+
+  it('renders navigation items and user', () => {
+    const { container } = render(<NavigationBar items={items} user={user} />);
+
+    // Assert snapshot
+    expect(container.firstChild).toMatchSnapshot();
+
+    // Assert navigation items
+    items.forEach((item) => {
+      const linkElement = screen.getByText(item.text);
+      expect(linkElement).toBeInTheDocument();
+      expect(linkElement).toHaveAttribute('href', item.href);
+    });
+
+    // Assert user
+    const userImage = screen.getByAltText(user.name);
+    expect(userImage).toBeInTheDocument();
+    expect(userImage).toHaveAttribute('src', user.src);
+  });
+
+  it('renders only navigation items', () => {
+    const { container } = render(<NavigationBar items={items} />);
+
+    // Assert snapshot
+    expect(container.firstChild).toMatchSnapshot();
+
+    // Assert navigation items
+    items.forEach((item) => {
+      const linkElement = screen.getByText(item.text);
+      expect(linkElement).toBeInTheDocument();
+      expect(linkElement).toHaveAttribute('href', item.href);
+    });
+
+    // Assert user is not rendered
+    const userImage = screen.queryByAltText(user.name);
+    expect(userImage).toBeNull();
+  });
+
+  it('renders only user', () => {
+    const { container } = render(<NavigationBar user={user} />);
+
+    // Assert snapshot
+    expect(container.firstChild).toMatchSnapshot();
+
+    // Assert user
+    const userImage = screen.getByAltText(user.name);
+    expect(userImage).toBeInTheDocument();
+    expect(userImage).toHaveAttribute('src', user.src);
+
+    // Assert navigation items are not rendered
+    items.forEach((item) => {
+      const linkElement = screen.queryByText(item.text);
+      expect(linkElement).toBeNull();
+    });
+  });
+
+  it('renders no navigation items and no user', () => {
+    const { container } = render(<NavigationBar />);
+
+    // Assert snapshot
+    expect(container.firstChild).toMatchSnapshot();
+
+    // Assert navigation items are not rendered
+    items.forEach((item) => {
+      const linkElement = screen.queryByText(item.text);
+      expect(linkElement).toBeNull();
+    });
+
+    // Assert user is not rendered
+    const userImage = screen.queryByAltText(user.name);
+    expect(userImage).toBeNull();
+  });
+});
