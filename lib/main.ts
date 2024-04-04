b6811ffb2ffc8bbdcb0e62d326d565eb953f8785
Sami Korpela <sami.korpela@gofore.com>
2024-04-04 14:58:56 +0300

    OPHJOD-312: Update ColorCard to be exported

--- lib/main.ts
+++ lib/main.ts
@@ -1,10 +1,11 @@
 import './index.css';
 
 export { Button } from './components/Button/Button';
+export { ColorCard } from './components/ColorCard/ColorCard';
 export { DropdownMenu } from './components/DropdownMenu/DropdownMenu';
-export { TextCard } from './components/TextCard/TextCard';
 export { NavigationBar } from './components/NavigationBar/NavigationBar';
 export { RadioButton } from './components/RadioButton/RadioButton';
 export { RadioButtonGroup } from './components/RadioButton/RadioButtonGroup';
 export { RoundButton } from './components/RoundButton/RoundButton';
 export { RoundLinkButton } from './components/RoundLinkButton/RoundLinkButton';
+export { TextCard } from './components/TextCard/TextCard';
