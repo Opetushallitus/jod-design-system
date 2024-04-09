675665ada03203053521ffcc86fd2e8f1099cce0
Sauli Anto <sauli.anto@gofore.com>
2024-04-09 08:10:50 +0300

    OPHJOD-296: Add toast variants

--- lib/components/Toast/Toast.tsx
+++ lib/components/Toast/Toast.tsx
@@ -5,17 +5,26 @@ export interface ToastProps {
   icon?: string;
   /** Aria label for the icon */
   iconAriaLabel?: string;
+  /** Variant of the toast */
+  variant?: 'success' | 'warning' | 'error';
 }
 
 /**
  * Toast component for displaying a text.
  */
-export const Toast = ({ text, icon, iconAriaLabel }: ToastProps) => (
+export const Toast = ({ text, icon, iconAriaLabel, variant = 'success' }: ToastProps) => (
   <div
     role="alert"
     aria-live="assertive"
     aria-atomic="true"
-    className={`inline-flex h-8 items-center gap-3 rounded-[4px] bg-success pl-5 text-body-sm ${icon ? 'pr-7' : 'pr-5'} text-white`}
+    className={`
+        ${variant === 'success' ? 'bg-success text-white' : ''}
+        ${variant === 'warning' ? 'bg-warning text-black' : ''}
+        ${variant === 'error' ? 'bg-alert text-white' : ''}
+        inline-flex h-8 items-center gap-3 rounded-[4px] pl-5
+        text-body-sm ${icon ? 'pr-7' : 'pr-5'}`
+      .replace(/\s+/g, ' ')
+      .trim()}
   >
     {icon && (
       <span className="material-symbols-outlined size-24 select-none" role="img" aria-label={iconAriaLabel}>
