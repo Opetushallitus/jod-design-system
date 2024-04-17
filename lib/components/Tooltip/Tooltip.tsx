28960b10631076c0afb6809f6958db33f798a0c0
Sami Korpela <sami.korpela@gofore.com>
2024-04-17 15:02:09 +0300

    OPHJOD-293: Add Tooltip component

---
+++ lib/components/Tooltip/Tooltip.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import type { Placement } from '@floating-ui/react';
+import { useTooltip } from './utils';
+
+export interface TooltipOptions {
+  initialOpen?: boolean;
+  placement?: Placement;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+}
+
+export interface TooltipProps extends TooltipOptions {
+  children?: React.ReactNode;
+}
+
+type ContextType = ReturnType<typeof useTooltip> | null;
+
+export const TooltipContext = React.createContext<ContextType>(null);
+
+export function Tooltip({ children, ...options }: TooltipProps) {
+  const tooltip = useTooltip(options);
+  return <TooltipContext.Provider value={tooltip}>{children}</TooltipContext.Provider>;
+}
---
+++ lib/components/Tooltip/utils.ts
@@ -0,0 +1,87 @@
+import React from 'react';
+import {
+  arrow,
+  autoUpdate,
+  flip,
+  offset,
+  shift,
+  useFloating,
+  useFocus,
+  useDismiss,
+  useHover,
+  useInteractions,
+  useRole,
+} from '@floating-ui/react';
+import { TooltipContext, TooltipOptions } from './Tooltip';
+
+export const ARROW_HEIGHT = 12;
+export const GAP = 0;
+
+export function useTooltip({
+  initialOpen = false,
+  placement = 'top',
+  open: controlledOpen,
+  onOpenChange: setControlledOpen,
+}: TooltipOptions = {}) {
+  const [uncontrolledOpen, setUncontrolledOpen] = React.useState(initialOpen);
+  const arrowRef = React.useRef(null);
+
+  const open = controlledOpen ?? uncontrolledOpen;
+  const setOpen = setControlledOpen ?? setUncontrolledOpen;
+
+  const data = useFloating({
+    placement,
+    open,
+    onOpenChange: setOpen,
+    whileElementsMounted: autoUpdate,
+    middleware: [
+      offset(ARROW_HEIGHT + GAP),
+      flip({
+        crossAxis: placement.includes('-'),
+        fallbackAxisSideDirection: 'start',
+        padding: 5,
+      }),
+      shift({ padding: 5 }),
+      arrow({
+        element: arrowRef,
+      }),
+    ],
+  });
+
+  const context = data.context;
+
+  const hover = useHover(context, {
+    move: false,
+    enabled: controlledOpen == null,
+  });
+
+  const focus = useFocus(context, {
+    enabled: controlledOpen == null,
+  });
+
+  const dismiss = useDismiss(context);
+  const role = useRole(context, { role: 'tooltip' });
+
+  const interactions = useInteractions([hover, focus, dismiss, role]);
+
+  return React.useMemo(
+    () => ({
+      open,
+      setOpen,
+      arrowRef,
+      ...interactions,
+      ...data,
+    }),
+    [open, setOpen, interactions, data],
+  );
+}
+
+export const useTooltipContext = () => {
+  const context = React.useContext(TooltipContext);
+
+  if (context == null) {
+    throw new Error('Tooltip components must be wrapped in <Tooltip />');
+  }
+
+  return context;
+};
