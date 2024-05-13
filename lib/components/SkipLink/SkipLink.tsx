3cf57133b0a9b522413cada45ca114a34b22fbd3
Sami Korpela <sami.korpela@gofore.com>
2024-05-13 11:32:15 +0300

    OPHJOD-421: Add margin for SkipLink

--- lib/components/SkipLink/SkipLink.tsx
+++ lib/components/SkipLink/SkipLink.tsx
@@ -7,7 +7,7 @@ export const SkipLink = ({ label, hash }: SkipLinkProps) => {
   return (
     <a
       href={hash}
-      className="absolute -left-full z-50 flex rounded-[4px] border-2 border-secondary-gray bg-white p-5 text-body-md text-button-sm text-link hover:text-accent hover:underline focus:left-auto focus:underline focus-visible:outline focus-visible:outline-[3px] focus-visible:outline-offset-[1.5px]"
+      className="absolute -left-full z-50 m-5 flex rounded-[4px] border-2 border-secondary-gray bg-white p-5 text-body-md text-button-sm text-link hover:text-accent hover:underline focus:left-auto focus:underline focus-visible:outline focus-visible:outline-[3px] focus-visible:outline-offset-[1.5px]"
     >
       {label}
     </a>
