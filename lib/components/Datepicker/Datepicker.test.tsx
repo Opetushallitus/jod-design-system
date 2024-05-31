c3717fc0bf7a6d41ed95ea280b4e3d3a7862acad
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2024-05-31 09:04:48 +0300

    OPHJOD-441: Datepicker component MVP

---
+++ lib/components/Datepicker/Datepicker.test.tsx
@@ -0,0 +1,52 @@
+import { userEvent } from '@storybook/test';
+import { act, render, screen } from '@testing-library/react';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+import { Datepicker } from './Datepicker';
+
+describe('Datepicker', () => {
+  const label = 'Pick a date dude!';
+  const placeholder = 'pp.kk.vvvv';
+  const value = ['2024-05-01'];
+  const formattedValue = '01.05.2024';
+  const onValueChange = vi.fn();
+
+  afterEach(() => {
+    onValueChange.mockClear();
+  });
+
+  it('renders correctly', () => {
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date(2024, 6, 1, 12, 0, 0));
+    const { container } = render(
+      <Datepicker label={label} placeholder={placeholder} value={value} onValueChange={onValueChange} />,
+    );
+    expect(container.firstChild).toMatchSnapshot();
+    vi.useRealTimers();
+  });
+
+  it('renders the correct label', () => {
+    render(<Datepicker label={label} placeholder={placeholder} value={value} onValueChange={onValueChange} />);
+    expect(screen.getByText(label)).toBeInTheDocument();
+  });
+
+  it('renders the correct initial value', () => {
+    render(<Datepicker label={label} placeholder={placeholder} value={value} onValueChange={onValueChange} />);
+    expect(screen.getByDisplayValue(formattedValue)).toBeInTheDocument();
+  });
+
+  it('calls onValueChange when a new date is typed into the input', async () => {
+    render(<Datepicker label={label} placeholder={placeholder} onValueChange={onValueChange} />);
+    const input = screen.getByRole('textbox');
+    const user = userEvent.setup();
+    await act(async () => {
+      await user.type(input, '01.06.2024');
+      await user.tab();
+    });
+    expect(onValueChange).toHaveBeenCalledWith(
+      expect.objectContaining({
+        value: [expect.objectContaining({ year: 2024, month: 6, day: 1 })],
+        valueAsString: ['2024-06-01'],
+      }),
+    );
+  });
+});
