e1b657e619fa64191d1d4857dcbf0a39c1f2be9c
Sami Korpela <sami.korpela@gofore.com>
2024-06-11 14:07:21 +0300

    OPHJOD-455: Update the Modal height and paddings

--- lib/components/Modal/Modal.tsx
+++ lib/components/Modal/Modal.tsx
@@ -17,16 +17,16 @@ export const Modal = ({ open, onClose, content, progress, sidePanel, footer }: M
   return (
     <Dialog open={open} onClose={onClose} className="relative z-50">
       <div className="fixed inset-0 bg-black/30" aria-hidden />
-      <div className="fixed inset-0 w-screen py-[96px]">
-        <div className="flex items-center justify-center">
-          <DialogPanel className="overflow-hidden rounded-[40px] border-[5px] border-inactive-gray bg-white">
-            <div className="my-6 grid max-h-[calc(100vh-320px)] max-w-[1092px] grid-cols-2 gap-6 pl-5 sm:grid-cols-6 sm:pl-9">
-              <div className="col-span-2 flex max-h-[calc(100vh-320px)] flex-col gap-6 pr-5 sm:col-span-4 sm:pr-0">
+      <div className="fixed inset-0 w-screen py-6 sm:py-[96px]">
+        <div className="flex h-full items-center justify-center">
+          <DialogPanel className="flex h-full flex-col justify-between overflow-hidden rounded-[40px] border-[3px] border-inactive-gray bg-white sm:border-[5px]">
+            <div className="my-6 grid max-h-[calc(100vh-172px)] max-w-[1092px] grid-cols-2 gap-6 pl-5 sm:max-h-[calc(100vh-320px)] sm:grid-cols-6 sm:pl-9">
+              <div className="col-span-2 flex max-h-[calc(100vh-172px)] flex-col gap-6 pr-5 sm:col-span-4 sm:max-h-[calc(100vh-320px)] sm:pr-0">
                 {progress && !sm && <div className="flex grow flex-col items-end">{progress}</div>}
                 <div className="overflow-y-auto sm:mt-6">{content}</div>
               </div>
               {sm && (
-                <div className="col-span-2 flex max-h-[calc(100vh-320px)] flex-col gap-6">
+                <div className="col-span-2 flex max-h-[calc(100vh-172px)] flex-col gap-6 sm:max-h-[calc(100vh-320px)]">
                   {progress && <div className="mr-6 flex grow flex-col items-end">{progress}</div>}
                   <div className="mr-9 overflow-y-auto">{sidePanel}</div>
                 </div>
