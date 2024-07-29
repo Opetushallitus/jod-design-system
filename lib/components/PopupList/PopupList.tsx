8a404431ff4d09737dd1c5c410355dfa716f7906
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2024-07-29 13:25:13 +0300

    OPHJOD-584: Refactor popupList to be more of a wrapper type component

--- lib/components/PopupList/PopupList.tsx
+++ lib/components/PopupList/PopupList.tsx
@@ -1,41 +1,26 @@
-export interface PopupListItem {
-  /** List item label */
-  label: string;
-  /** Is list item active */
-  active?: boolean;
-  /** Optional click handler */
-  onClick?: () => void;
-  /** Link destination */
-  href?: string;
-  /** Type of the element. Default is link */
-  type?: 'button' | 'link';
-}
-
 export interface PopupListProps {
-  /** List items */
-  items: PopupListItem[];
+  children: React.ReactNode;
+  classNames?: string;
 }
 
-export const PopupList = ({ items }: PopupListProps) => {
-  return (
-    <div className="inline-flex flex-col items-start rounded bg-white py-6 px-[20px] shadow-border w-[257px]">
-      <ul className="w-full">
-        {items.map((item) => {
-          const Component = item.type === 'button' ? 'button' : 'a';
+export type PopupListItemProps = PopupListProps;
 
-          return (
-            <li key={item.label}>
-              <Component
-                {...(Component === 'button' ? { type: 'button', onClick: item.onClick } : { href: item.href ?? '#' })}
-                aria-current={item.active ? 'true' : undefined}
-                className={`flex items-center gap-3 py-3 text-heading-4 font-poppins hover:text-accent hover:underline focus:left-auto focus:underline w-full ${item.active ? 'bg-secondary-1-50' : ''} pl-5 rounded`}
-              >
-                {item.label}
-              </Component>
-            </li>
-          );
-        })}
-      </ul>
+export const PopupList = ({ children, classNames = '' }: PopupListProps) => {
+  return (
+    <div
+      className={`inline-flex flex-col items-start rounded bg-white py-6 px-[20px] shadow-border w-[257px] ${classNames}`.trim()}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const PopupListItem = ({ children, classNames = '' }: PopupListItemProps) => {
+  return (
+    <div
+      className={`flex items-center gap-3 py-3 text-heading-4 font-poppins hover:text-accent hover:underline focus:left-auto focus:underline w-full pl-5 rounded ${classNames}`.trim()}
+    >
+      {children}
     </div>
   );
 };
