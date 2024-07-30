f8e7dd1f948decd3aac87fc1e768625b7f0ae10a
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2024-07-30 14:11:01 +0300

    OPHJOD-549: HeroCard design update

--- lib/components/HeroCard/HeroCard.test.tsx
+++ lib/components/HeroCard/HeroCard.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { createEvent, fireEvent, render, screen } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
 
 import { HeroCard } from './HeroCard';
 
@@ -54,11 +54,61 @@ describe('HeroCard', () => {
     expect(screen.queryByText(actionContent)).toBeNull();
   });
 
-  it('renders HeroCard with a link when actionContent is not provided', () => {
-    render(<HeroCard title={title} content={content} backgroundColor={backgroundColor} href="/" />);
+  it('renders the arrow by default if either href or actionContent is present', () => {
+    const defaultProps = { title, content, backgroundColor };
+    const { rerender } = render(<HeroCard {...defaultProps} />);
+
+    // Assert arrow is not rendered
+    expect(document.querySelector('svg')).toBeNull();
+
+    // Assert arrow is rendered when href is provided
+    rerender(<HeroCard {...defaultProps} href="/" />);
+    expect(document.querySelector('svg')).not.toBeNull();
+
+    // Assert arrow is rendered when actionContent is provided
+    rerender(<HeroCard {...defaultProps} actionContent={actionContent} />);
+    expect(document.querySelector('svg')).not.toBeNull();
+  });
+
+  it('renders HeroCard with a link when size is sm', () => {
+    render(<HeroCard title={title} content={content} backgroundColor={backgroundColor} href="/" size="sm" />);
 
     // Assert HeroCard is wrapped in a link
     const linkElement = screen.getByRole('link');
     expect(linkElement).toBeInTheDocument();
+  });
+
+  it('handles link clicks normally when onClick is not provided', () => {
+    render(<HeroCard title={title} content={content} backgroundColor={backgroundColor} href="/" size="sm" />);
+
+    // Assert HeroCard is wrapped in a link
+    const linkElement = screen.getByRole('link');
+    expect(linkElement).toBeInTheDocument();
+    const clickEvent = createEvent.click(linkElement);
+    fireEvent(linkElement, clickEvent);
+    expect(clickEvent.defaultPrevented).toBe(false);
+  });
+
+  it('prevents the default link click event when onclick is passed', () => {
+    const onClick = vi.fn();
+    render(
+      <HeroCard
+        title={title}
+        content={content}
+        backgroundColor={backgroundColor}
+        href="/"
+        size="sm"
+        onClick={onClick}
+      />,
+    );
+
+    // Assert HeroCard is wrapped in a link
+    const linkElement = screen.getByRole('link');
+    expect(linkElement).toBeInTheDocument();
+
+    const clickEvent = createEvent.click(linkElement);
+    fireEvent(linkElement, clickEvent);
+    expect(onClick).toHaveBeenCalled();
+    expect(clickEvent.defaultPrevented).toBe(true);
   });
 });
--- lib/components/HeroCard/HeroCard.tsx
+++ lib/components/HeroCard/HeroCard.tsx
@@ -8,56 +8,89 @@ export interface HeroCardProps {
   /** Background color of the card */
   backgroundColor?: string;
   /** Link to page */
-  href: string;
+  href?: string;
+  /** Text color */
+  textColor?: string;
+  /** Size of the card. Lg is default. */
+  size?: 'lg' | 'sm';
+  /** Arrow visibility. By default it's true if href or actionContent is present. */
+  arrowVisible?: boolean;
+  /** Arrow color. Defaults to text color */
+  arrowColor?: string;
+  /** Override the default link click event handling */
+  onClick?: () => void;
 }
 
-export const HeroCard = ({ title, content, actionContent, backgroundColor = '#444BACF2', href }: HeroCardProps) => (
-  <>
-    {actionContent ? (
-      <BaseCard
-        title={title}
-        content={content}
-        actionContent={actionContent}
-        backgroundColor={backgroundColor}
-        href={href}
-      />
-    ) : (
-      <a href={href} className="flex rounded-[28px] outline-none transition-transform hover:scale-105 focus:scale-105">
-        <BaseCard title={title} content={content} backgroundColor={backgroundColor} href={href} />
-      </a>
-    )}
-  </>
+const Arrow = ({ color }: { color: string }) => (
+  <div className="size-32 ml-auto select-none font-bold" aria-hidden>
+    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+      <path d="M30 38L27.15 35.2L36.35 26H4V22H36.35L27.2 12.8L30 10L44 24L30 38Z" fill={color} />
+    </svg>
+  </div>
 );
 
-const BaseCard = ({ title, content, actionContent, backgroundColor, href }: HeroCardProps) => {
-  const Heading = actionContent ? 'h1' : 'h2';
-  return (
-    <div
-      className={`relative flex flex-col gap-6 rounded-[28px] px-7 pb-11 pt-7 text-white`}
-      style={{ backgroundColor }}
-    >
-      <Heading className="overflow hyphens-auto text-pretty text-[28px] font-bold leading-[34px]">{title}</Heading>
-      {content && <p>{content}</p>}
-      {actionContent ? (
-        <a
-          href={href}
-          className="absolute bottom-0 right-7 translate-y-2/4 rounded-[40px] outline-none transition-transform hover:scale-105 focus:scale-105"
-        >
-          <div
-            className="flex select-none items-center gap-4 rounded-[40px] bg-white px-8 py-[20px] text-[28px] font-bold leading-[40px]"
-            style={{ color: backgroundColor }}
+export const HeroCard = ({
+  title,
+  content,
+  actionContent,
+  backgroundColor = '#444BACF2',
+  href,
+  textColor = '#FFF',
+  arrowColor = textColor,
+  arrowVisible = !!href || !!actionContent,
+  size = 'lg',
+  onClick,
+}: HeroCardProps) => {
+  const handleLinkClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (onClick) {
+      e?.preventDefault?.();
+      onClick();
+    }
+  };
+
+  if (size === 'lg') {
+    return (
+      <div
+        className="font-poppins relative flex flex-col gap-6 rounded-lg px-7 pb-11 pt-7 tracking-widest"
+        style={{ backgroundColor, color: textColor }}
+      >
+        <h1 className="overflow text-pretty text-[36px] font-semibold leading-[50px]">{title}</h1>
+        {content && <p className="tracking-normal leading-[27px] font-medium text-[18px]">{content}</p>}
+        {actionContent && (
+          <a
+            href={href}
+            className="absolute bottom-0 right-7 translate-y-2/4 rounded-[40px] outline-none transition-transform hover:scale-105 focus:scale-105"
+            onClick={handleLinkClick}
           >
-            {actionContent}
-            <span className="material-symbols-outlined size-32 select-none font-bold" aria-hidden>
-              arrow_forward
-            </span>
-          </div>
-        </a>
-      ) : (
-        <span className="material-symbols-outlined size-32 absolute bottom-5 right-7 select-none font-bold" aria-hidden>
-          arrow_forward
-        </span>
-      )}
-    </div>
-  );
+            <div
+              className="flex select-none items-center gap-4 rounded-[40px] bg-white px-8 py-[20px] text-[28px] font-bold leading-[40px]"
+              style={{ color: backgroundColor }}
+            >
+              {actionContent}
+              {arrowVisible && <Arrow color={arrowColor} />}
+            </div>
+          </a>
+        )}
+        {!actionContent && arrowVisible && <Arrow color={arrowColor} />}
+      </div>
+    );
+  }
+
+  if (size === 'sm') {
+    return (
+      <a
+        href={href}
+        className="flex rounded-lg outline-none transition-transform hover:scale-105 focus:scale-105"
+        onClick={handleLinkClick}
+      >
+        <div
+          className="font-poppins relative flex flex-col gap-6 rounded-lg p-7 pb-6 tracking-widest justify-between"
+          style={{ backgroundColor, color: textColor }}
+        >
+          <h2 className="overflow text-pretty text-[24px] font-semibold leading-[34px]">{title}</h2>
+          {arrowVisible && <Arrow color={arrowColor} />}
+        </div>
+      </a>
+    );
+  }
 };
