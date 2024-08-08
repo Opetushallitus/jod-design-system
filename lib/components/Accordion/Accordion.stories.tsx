a590cba4deeee3d162cd15af4e988de87bf552fc
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2024-08-08 15:51:40 +0300

    OPHJOD-556: Accordion design update

--- lib/components/Accordion/Accordion.stories.tsx
+++ lib/components/Accordion/Accordion.stories.tsx
@@ -12,21 +12,74 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+// Decorator for preventing the preview size change when the content is toggled
+const decorators: Story['decorators'] = [
+  (Story) => (
+    <div className="min-h-[80px]">
+      <Story />
+    </div>
+  ),
+];
+
+const parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/design/6M2LrpSCcB0thlFDaQAI2J/cx_jod_client?node-id=2217-6882',
+  },
+};
+
 export const Default: Story = {
+  decorators,
   parameters: {
-    design: {
-      type: 'figma',
-      url: 'https://www.figma.com/design/6M2LrpSCcB0thlFDaQAI2J/cx_jod_client?node-id=541-11408',
-    },
+    ...parameters,
     docs: {
       description: {
-        story: 'This is XYZ',
+        story: 'Accordion component for hiding content.',
       },
     },
   },
   args: {
     title: 'Title',
     children: 'Content',
+    lang: 'en',
+    expandMoreText: 'Show more',
+    expandLessText: 'Show less',
+  },
+};
+
+export const TitleComponent: Story = {
+  decorators,
+  parameters: {
+    ...parameters,
+    docs: {
+      description: {
+        story: 'Accordion component with a custom component as a title.',
+      },
+    },
+  },
+  args: {
+    title: <div className="text-heading-1 font-arial text-alert italic tracking-widest">Title</div>,
+    children: 'Content',
+    lang: 'en',
+    expandMoreText: 'Show more',
+    expandLessText: 'Show less',
+  },
+};
+
+export const WithUnderline: Story = {
+  decorators,
+  parameters: {
+    ...parameters,
+    docs: {
+      description: {
+        story: 'Accordion component with underlined title.',
+      },
+    },
+  },
+  args: {
+    title: 'Title',
+    children: 'Content',
+    underline: true,
     lang: 'en',
     expandMoreText: 'Show more',
     expandLessText: 'Show less',
