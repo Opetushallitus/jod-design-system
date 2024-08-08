638185c1693450267f43b363611f77341fc5ff5d
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2024-08-08 15:51:40 +0300

    OPHJOD-556: Accordion design update

--- lib/components/Accordion/Accordion.tsx
+++ lib/components/Accordion/Accordion.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { cx } from '../../cva';
 
 interface AccordionProps {
   title: React.ReactNode | string;
@@ -6,13 +7,27 @@ interface AccordionProps {
   expandLessText: string;
   expandMoreText: string;
   lang: string;
+  underline?: boolean;
 }
 
 const INITIAL_STATE = true;
 
-export const Accordion = ({ title, children, expandLessText, expandMoreText, lang }: AccordionProps) => {
+const Caret = ({ isOpen }: { isOpen: boolean }) => (
+  <span
+    className="material-symbols-outlined size-32 select-none text-secondary-gray group-hover:!text-accent"
+    aria-hidden
+  >
+    {isOpen ? 'expand_less' : 'expand_more'}
+  </span>
+);
+
+export const Accordion = ({ title, children, expandLessText, expandMoreText, lang, underline }: AccordionProps) => {
   const [isOpen, setIsOpen] = React.useState(INITIAL_STATE);
+  const toggleOpen = () => setIsOpen(!isOpen);
   const isTitleValidElement = React.isValidElement(title);
+  const wrapperClassnames = cx('flex w-full items-center justify-between gap-x-4 mb-2 group-hover:!text-accent', {
+    'border-b border-border-gray': underline,
+  });
 
   // Reset the state when the children change
   React.useEffect(() => {
@@ -22,35 +37,28 @@ export const Accordion = ({ title, children, expandLessText, expandMoreText, lan
   return (
     <>
       {isTitleValidElement ? (
-        <div className="flex w-full items-center justify-between gap-x-4">
+        <div className={wrapperClassnames}>
           {title}
-          <button
-            aria-label={isOpen ? expandLessText : expandMoreText}
-            onClick={() => {
-              setIsOpen(!isOpen);
-            }}
-            className="flex"
-          >
-            <span className="material-symbols-outlined size-32 select-none font-bold" aria-hidden>
-              {isOpen ? 'expand_less' : 'expand_more'}
-            </span>
+          <button aria-label={isOpen ? expandLessText : expandMoreText} onClick={toggleOpen} className="flex">
+            <Caret isOpen={isOpen} />
           </button>
         </div>
       ) : (
-        <button
-          aria-label={isOpen ? expandLessText : expandMoreText}
-          onClick={() => {
-            setIsOpen(!isOpen);
-          }}
-          className="flex w-full items-center justify-between gap-x-4"
-        >
-          <div className="hyphens-auto text-heading-4 font-poppins" lang={lang}>
-            {title}
-          </div>
-          <span className="material-symbols-outlined size-32 select-none font-bold" aria-hidden>
-            {isOpen ? 'expand_less' : 'expand_more'}
-          </span>
-        </button>
+        <div className="group">
+          <button
+            aria-label={isOpen ? expandLessText : expandMoreText}
+            onClick={toggleOpen}
+            className={wrapperClassnames}
+          >
+            <span
+              className="mr-5 w-full text-left hyphens-auto text-heading-3 font-poppins group-hover:underline"
+              lang={lang}
+            >
+              {title}
+            </span>
+            <Caret isOpen={isOpen} />
+          </button>
+        </div>
       )}
       {isOpen && children}
     </>
