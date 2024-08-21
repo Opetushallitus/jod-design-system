58dc7c15d9f29c250e57c56daf8f1b86f98dba3a
Sami Korpela <sami.korpela@gofore.com>
2024-08-21 15:30:21 +0300

    OPHJOD-633: Update RadioButtonGroup label to be required + add hideLabel prop

--- lib/components/RadioButton/RadioButtonGroup.tsx
+++ lib/components/RadioButton/RadioButtonGroup.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 
 export interface RadioButtonGroupProps {
   /** Text for the component */
-  label?: string;
+  label: string;
+  /** Hide label */
+  hideLabel?: boolean;
   /** Value that is currently selected */
   value: string;
   /** Callback function when the value changes */
@@ -15,14 +17,23 @@ export interface RadioButtonGroupProps {
 }
 
 /** Radio buttons allow users to select a single option from a list of mutually exclusive options. All possible options are exposed up front for users to compare. */
-export const RadioButtonGroup = ({ label, value, onChange, children, className }: RadioButtonGroupProps) => {
+export const RadioButtonGroup = ({
+  label,
+  hideLabel = false,
+  value,
+  onChange,
+  children,
+  className,
+}: RadioButtonGroupProps) => {
   return (
     <RadioGroup
       value={value}
       onChange={onChange}
       className={`${className ? className : ''} ds-flex ds-flex-col ds-space-y-2`.trim()}
     >
-      {label && <Label className="ds-mb-5 ds-text-body-xs ds-text-secondary-gray">{label}</Label>}
+      <Label className={`ds-mb-5 ds-text-body-xs ds-text-secondary-gray ${hideLabel ? 'ds-hidden' : ''}`.trim()}>
+        {label}
+      </Label>
       {children}
     </RadioGroup>
   );
