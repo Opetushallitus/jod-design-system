5261aba192ecd7c5a9a22cb08eb03211e1f7ea37
Markus Boman <markus.boman@elenium.fi>
2024-12-03 08:25:47 +0200

    OPHJOD-1066: Changed tooltip logic

--- lib/components/SelectionCard/SelectionCard.stories.tsx
+++ lib/components/SelectionCard/SelectionCard.stories.tsx
@@ -1,7 +1,9 @@
 import { useState } from '@storybook/preview-api';
 import type { Meta, StoryObj } from '@storybook/react';
+import { MdInfoOutline } from 'react-icons/md';
+import { JSX } from 'react/jsx-runtime';
 import { useMediaQueries } from '../../main';
-import { SelectionCard } from './SelectionCard';
+import { SelectionCard, SelectionCardProps } from './SelectionCard';
 
 const meta = {
   title: 'Cards/SelectionCard',
@@ -20,21 +22,24 @@ const parameters = {
   },
 };
 
-export const Default: Story = {
-  render: (args) => {
-    const [selected, setSelected] = useState(false);
-    const onClick = () => {
-      setSelected(!selected);
-    };
+const RenderDefault = (args: JSX.IntrinsicAttributes & SelectionCardProps) => {
+  const [selected, setSelected] = useState(false);
+  const onClick = () => {
+    setSelected(!selected);
+  };
 
-    return <SelectionCard {...args} selected={selected} onClick={onClick} />;
-  },
+  return <SelectionCard {...args} selected={selected} onClick={onClick} />;
+};
+
+export const Default: Story = {
+  render: RenderDefault,
   parameters,
   args: {
     label: 'Kartoittaa omaa osaamistani',
     icon: <span className="ds-text-[70px]">😎</span>,
     selected: false,
     infoAriaLabel: 'Info',
+    infoIcon: <MdInfoOutline size={24} />,
     tooltipContent: (
       <>
         <div className="ds-text-heading-4 ds-mb-4">Tavoitteen vaikutus tuloksiin</div>
@@ -46,66 +51,69 @@ export const Default: Story = {
     ),
   },
 };
+
+const RenderMultiple = () => {
+  const { sm } = useMediaQueries();
+  const [cardData, setCardData] = useState([
+    { icon: '🙈', selected: false, label: 'Lorem ipsum' },
+    { icon: '🙉', selected: false, label: 'Dolor sit amet' },
+    { icon: '🙊', selected: false, label: 'Consectetur adipiscing elit' },
+  ]);
+
+  const [info, setInfo] = useState('');
+
+  const onClick = (index: number) => () => {
+    setCardData((prev) => {
+      const data = [...prev];
+      data[index].selected = !data[index].selected;
+      return data;
+    });
+  };
+
+  const setInfoVisible = (index: number) => (visible: boolean) => {
+    // eslint-disable-next-line sonarjs/no-selector-parameter
+    if (visible && info !== cardData[index].label) {
+      setInfo(cardData[index].label);
+    } else if (!visible) {
+      setInfo('');
+    }
+  };
+
+  const CardList = () =>
+    cardData.map((card, index) => (
+      <SelectionCard
+        key={card.label}
+        label={card.label}
+        selected={card.selected}
+        onClick={onClick(index)}
+        setHovered={setInfoVisible(index)}
+        icon={<span className="ds-text-[70px]">{card.icon}</span>}
+        infoAriaLabel={`Info for ${card.label}`}
+        tooltipContent={
+          <div>
+            Tooltip for <strong>{card.label}</strong>
+          </div>
+        }
+      />
+    ));
+  return (
+    <>
+      {sm && <div>Showing info for card: {info}</div>}
+      {sm ? (
+        <div className="ds-flex ds-flex-row ds-gap-x-5">
+          <CardList />
+        </div>
+      ) : (
+        <div className="ds-flex ds-flex-col ds-gap-y-5">
+          <CardList />
+        </div>
+      )}
+    </>
+  );
+};
+
 export const MultipleWithHover: Story = {
-  render: () => {
-    const { sm } = useMediaQueries();
-    const [cardData, setCardData] = useState([
-      { icon: '🙈', selected: false, label: 'Lorem ipsum' },
-      { icon: '🙉', selected: false, label: 'Dolor sit amet' },
-      { icon: '🙊', selected: false, label: 'Consectetur adipiscing elit' },
-    ]);
-
-    const [info, setInfo] = useState('');
-
-    const onClick = (index: number) => () => {
-      setCardData((prev) => {
-        const data = [...prev];
-        data[index].selected = !data[index].selected;
-        return data;
-      });
-    };
-
-    const setInfoVisible = (index: number) => (visible: boolean) => {
-      // eslint-disable-next-line sonarjs/no-selector-parameter
-      if (visible && info !== cardData[index].label) {
-        setInfo(cardData[index].label);
-      } else if (!visible) {
-        setInfo('');
-      }
-    };
-
-    const CardList = () =>
-      cardData.map((card, index) => (
-        <SelectionCard
-          key={card.label}
-          label={card.label}
-          selected={card.selected}
-          onClick={onClick(index)}
-          setHovered={setInfoVisible(index)}
-          icon={<span className="ds-text-[70px]">{card.icon}</span>}
-          infoAriaLabel={`Info for ${card.label}`}
-          tooltipContent={
-            <div>
-              Tooltip for <strong>{card.label}</strong>
-            </div>
-          }
-        />
-      ));
-    return (
-      <>
-        {sm && <div>Showing info for card: {info}</div>}
-        {sm ? (
-          <div className="ds-flex ds-flex-row ds-gap-x-5">
-            <CardList />
-          </div>
-        ) : (
-          <div className="ds-flex ds-flex-col ds-gap-y-5">
-            <CardList />
-          </div>
-        )}
-      </>
-    );
-  },
+  render: RenderMultiple,
   parameters: {
     ...parameters,
   },
