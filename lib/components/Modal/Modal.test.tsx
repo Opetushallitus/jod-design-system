5ddaa15a08c1c927a8c8ad2ea7eed24caaa9f7e1
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2025-02-26 12:12:24 +0200

    OPHJOD-851: Update ark UI and suppress act warning modal test

--- lib/components/Modal/Modal.test.tsx
+++ lib/components/Modal/Modal.test.tsx
@@ -1,22 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import { expect, test, vi } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
 import { Modal } from './Modal';
 
-test('Modal renders correctly', () => {
-  const content = <div>Modal Content</div>;
-  const footer = <div>Modal Footer</div>;
+describe('Modal', () => {
+  it('renders correctly', async () => {
+    const content = <div>Modal Content</div>;
+    const footer = <div>Modal Footer</div>;
 
-  render(<Modal open={true} onClose={vi.fn()} content={content} footer={footer} />);
+    render(<Modal open={true} onClose={vi.fn()} content={content} footer={footer} />);
 
-  // Assert that the modal is rendered
-  const modalElement = screen.getByRole('dialog');
-  expect(modalElement).toBeInTheDocument();
+    // Assert that the modal is rendered
+    const modalElement = screen.getByRole('dialog');
+    await waitFor(() => expect(modalElement).toBeInTheDocument());
 
-  // Assert that the content is rendered
-  const contentElement = screen.getByText('Modal Content');
-  expect(contentElement).toBeInTheDocument();
+    // Assert that the content is rendered
+    const contentElement = screen.getByText('Modal Content');
+    expect(contentElement).toBeInTheDocument();
 
-  // Assert that the footer is rendered
-  const footerElement = screen.getByText('Modal Footer');
-  expect(footerElement).toBeInTheDocument();
+    // Assert that the footer is rendered
+    const footerElement = screen.getByText('Modal Footer');
+    expect(footerElement).toBeInTheDocument();
+  });
 });
