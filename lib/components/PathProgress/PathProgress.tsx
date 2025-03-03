ad1bcffd6c43ae7c1cda9b9b05ce6f5fcad3692e
Sami Korpela <sami.korpela@gofore.com>
2025-03-03 11:36:20 +0200

    OPHJOD-1337: Update PathProgress component

--- lib/components/PathProgress/PathProgress.tsx
+++ lib/components/PathProgress/PathProgress.tsx
@@ -1,77 +1,61 @@
 import React from 'react';
-import { GrTarget } from 'react-icons/gr';
-import { MdCheck, MdOutlineFlag } from 'react-icons/md';
 import { tidyClasses as tc } from '../../utils';
 
-export interface PathProgressProps {
-  steps: number;
-  currentStep: number;
-  selectedStep?: number;
-  onClick?: (step: number) => void;
-}
+type StepLabel =
+  | {
+      labelComponent: React.ReactNode;
+      label?: never;
+    }
+  | {
+      labelComponent?: never;
+      label: string;
+    };
 
-export const PathProgress = ({ steps = 5, currentStep = 0, selectedStep, onClick }: PathProgressProps) => {
-  const getStep = React.useCallback((step: number) => Math.min(Math.max(step, 0), steps + 1), [steps]);
-  const [step, setStep] = React.useState(getStep(currentStep));
+export type PathProgressStep = {
+  /** text or icon inside the circle */
+  circleComponent: React.ReactNode;
+  content?: React.ReactNode;
+  isCompleted?: boolean;
+} & StepLabel;
 
-  React.useEffect(() => {
-    setStep(getStep(currentStep));
-  }, [currentStep, getStep]);
-
-  const baseClasses = 'ds:size-8 ds:rounded-full ds:appearance-none ds:z-10 ds:flex ds:items-center ds:justify-center';
-  const getActiveClasses = (idx: number) =>
-    step === idx ? 'ds:bg-accent ds:text-white' : 'ds:bg-bg-gray-2 ds:text-black';
-
-  const handleOnClick = (idx: number) => () => {
-    setStep(idx);
-    onClick?.(idx);
-  };
+const StepComponent = ({ step, isLast }: { step: PathProgressStep; isLast: boolean }) => {
+  const baseClasses = tc(['ds:flex ds:flex-row ds:relative ds:h-full ds:w-full', isLast ? '' : 'ds:pb-9']);
+  const lineClasses = tc([
+    'ds:absolute ds:top-1 ds:transform ds:left-[18px] ds:w-2 ds:h-full',
+    step.isCompleted ? 'ds:bg-accent' : 'ds:bg-white',
+  ]);
+  const circleClasses = tc([
+    'ds:min-w-8 ds:size-8 ds:rounded-full ds:appearance-none ds:z-10 ds:flex ds:items-center ds:justify-center ds:mb-6 ds:text-button-lg',
+    step.isCompleted ? 'ds:bg-accent ds:text-white' : 'ds:bg-white ds:text-black',
+  ]);
 
   return (
+    <div className={baseClasses}>
+      {!isLast && <span className={lineClasses} />}
+      <span className={circleClasses}>{step.circleComponent}</span>
+      <div className="ds:flex ds:flex-col ds:ml-7 ds:mt-3 ds:gap-5 ds:w-full">
+        {step.label && <span className="ds:text-heading-2">{step.label}</span>}
+        {step.labelComponent && <div>{step.labelComponent}</div>}
+        {step.content && <div>{step.content}</div>}
+      </div>
+    </div>
+  );
+};
+
+export interface PathProgressProps {
+  steps: PathProgressStep[];
+}
+
+export const PathProgress = ({ steps }: PathProgressProps) => {
+  return (
     <div className="ds:inline-flex ds:flex-col ds:h-full ds:place-content-between ds:relative">
-      <span className="ds:absolute ds:top-0 ds:left-[50%] ds:transform ds:-translate-x-[50%] ds:bg-white ds:w-2 ds:h-full" />
-      <button
-        type="button"
-        className={tc(`
-          ds:cursor-pointer
-          ds:mb-6
-          ${baseClasses}
-          ${getActiveClasses(0)}
-        `)}
-        onClick={handleOnClick(0)}
-      >
-        <MdOutlineFlag size={24} />
-      </button>
-      {Array.from({ length: steps }).map((_, index) => (
-        <button
-          type="button"
+      {steps.map((step, index) => (
+        <StepComponent
           key={index} // eslint-disable-line react/no-array-index-key
-          className={tc([
-            'ds:cursor-pointer',
-            'ds:text-button-lg',
-            'ds:mb-6',
-            baseClasses,
-            getActiveClasses(index + 1),
-            index < 2 ? 'ds:mt-auto' : '',
-            selectedStep === index + 1 ? 'ds:flex ds:items-center ds:justify-center' : '',
-          ])}
-          onClick={handleOnClick(index + 1)}
-        >
-          {selectedStep === index + 1 ? <MdCheck size={24} /> : index + 1}
-        </button>
+          step={step}
+          isLast={index == steps.length - 1}
+        />
       ))}
-      <button
-        type="button"
-        className={tc(`
-          ds:cursor-pointer
-          ${baseClasses}
-          ${getActiveClasses(steps + 1)}
-          ds:mb-0
-        `)}
-        onClick={handleOnClick(steps + 1)}
-      >
-        <GrTarget size={24} />
-      </button>
     </div>
   );
 };
