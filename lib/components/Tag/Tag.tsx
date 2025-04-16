4327d3c64166295d0041ebeb502be5e4c34985ee
Nhut Do <nhut.do@elenium.fi>
2025-04-16 11:00:00 +0300

    OPHJOD-1451: Change Tag component cursor behavior for 'presentation'
    Change the Tag component so that when variant === 'presentation', it doesn't appear clickable (i.e., no pointer cursor).

--- lib/components/Tag/Tag.tsx
+++ lib/components/Tag/Tag.tsx
@@ -20,10 +20,11 @@ interface ActionableTagProps extends BaseTagProps {
 
 export type TagProps = PresentationTagProps | ActionableTagProps;
 
-const containerClassNames = (sourceType: TagProps['sourceType']) =>
+const containerClassNames = (sourceType: TagProps['sourceType'], variant: TagProps['variant']) =>
   cx(
-    'ds:cursor-pointer ds:group ds:inline-flex ds:select-none ds:items-center ds:rounded-xl ds:text-tag ds:font-arial ds:px-4 ds:py-2 ds:text-left',
+    'ds:group ds:inline-flex ds:select-none ds:items-center ds:rounded-xl ds:text-tag ds:font-arial ds:px-4 ds:py-2 ds:text-left',
     {
+      'ds:cursor-pointer': variant !== 'presentation',
       'ds:bg-tag-tyopaikka': sourceType === 'tyopaikka',
       'ds:bg-tag-koulutus': sourceType === 'koulutus',
       'ds:bg-tag-vapaa-ajan-toiminto': sourceType === 'vapaa-ajan-toiminto',
@@ -36,11 +37,11 @@ const containerClassNames = (sourceType: TagProps['sourceType']) =>
 /** Tags allow users to categorize content. They can represent keywords or people, and are grouped to describe an item or a search request. */
 export const Tag = ({ label, title, onClick, variant = 'selectable', sourceType = 'jotain-muuta' }: TagProps) => {
   return variant === 'presentation' ? (
-    <div className={containerClassNames(sourceType)} title={title}>
+    <div className={containerClassNames(sourceType, variant)} title={title}>
       <span className="ds:hyphens-auto ds:text-black">{label}</span>
     </div>
   ) : (
-    <button type="button" className={containerClassNames(sourceType)} onClick={onClick} title={title}>
+    <button type="button" className={containerClassNames(sourceType, variant)} onClick={onClick} title={title}>
       <span className="ds:hyphens-auto ds:text-black ds:group-hover:underline">{label}</span>
       <span className="ds:pl-3 ds:text-button-md ds:text-black" aria-hidden>
         {variant === 'selectable' ? <MdAdd size={16} /> : <MdClose size={16} />}
