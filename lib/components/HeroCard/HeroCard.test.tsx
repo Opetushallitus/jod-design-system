284a749f8cbe3f0f62c412ae4f59b71f1a424dce
Sami Korpela <sami.korpela@gofore.com>
2025-05-15 11:45:11 +0300

    OPHJOD-1583: Update HeroCard

--- lib/components/HeroCard/HeroCard.test.tsx
+++ lib/components/HeroCard/HeroCard.test.tsx
@@ -2,104 +2,49 @@ import '@testing-library/jest-dom';
 import { createEvent, fireEvent, render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
-import { HeroCard, HeroCardProps } from './HeroCard';
+import { HeroCard } from './HeroCard';
 
 describe('HeroCard', () => {
   const title = 'Test Title';
   const content = 'Test Content';
-  const actionContent = 'Test Action Content';
+  const buttonLabel = 'Test Button Label';
   const backgroundColor = '#444BACF2';
 
-  it('renders HeroCard with actionContent', () => {
-    render(
+  it('renders HeroCard with a button', () => {
+    const { container } = render(
       <HeroCard
         to="/"
-        linkComponent={({ to, children }) => <a href={to as string}>{children}</a>}
+        LinkComponent={({ to, children }) => <a href={to as string}>{children}</a>}
+        buttonLabel={buttonLabel}
         title={title}
         content={content}
-        actionContent={actionContent}
         backgroundColor={backgroundColor}
       />,
     );
 
-    // Assert title, content, and actionContent are rendered
     expect(screen.getByText(title)).toBeInTheDocument();
-    expect(screen.getByText(actionContent)).toBeInTheDocument();
-  });
-
-  it('renders HeroCard with a link when actionContent is provided', () => {
-    render(
-      <HeroCard
-        to="/"
-        linkComponent={({ to, children }) => <a href={to as string}>{children}</a>}
-        title={title}
-        content={content}
-        actionContent={actionContent}
-        backgroundColor={backgroundColor}
-      />,
-    );
-
-    // Assert HeroCard is wrapped in a link
+    expect(screen.getByText(buttonLabel)).toBeInTheDocument();
     const linkElement = screen.getByRole('link');
     expect(linkElement).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 
-  it('renders HeroCard without actionContent', () => {
-    render(
-      <HeroCard
-        to="/"
-        linkComponent={({ to, children }) => <a href={to as string}>{children}</a>}
-        title={title}
-        content={content}
-        backgroundColor={backgroundColor}
-      />,
-    );
+  it('renders HeroCard without a button', () => {
+    const { container } = render(<HeroCard title={title} content={content} backgroundColor={backgroundColor} />);
 
-    // Assert title and content are rendered
     expect(screen.getByText(title)).toBeInTheDocument();
+    expect(screen.queryByText(buttonLabel)).toBeNull();
 
-    // Assert actionContent is not rendered
-    expect(screen.queryByText(actionContent)).toBeNull();
-  });
-
-  it('renders the arrow by default if to and linkComponent and actionContent are present', () => {
-    const defaultProps: HeroCardProps = {
-      title,
-      content,
-      backgroundColor,
-    };
-    const { rerender } = render(<HeroCard {...defaultProps} />);
-
-    // Assert arrow is not rendered
-    expect(document.querySelector('svg')).toBeNull();
-
-    // Assert arrow is rendered when href is provided
-    rerender(
-      <HeroCard
-        {...defaultProps}
-        to={'/'}
-        linkComponent={({ to, children }) => <a href={to as string}>{children}</a>}
-      />,
-    );
-    expect(document.querySelector('svg')).not.toBeNull();
-
-    // Assert arrow is rendered when actionContent is provided
-    rerender(
-      <HeroCard
-        {...defaultProps}
-        to={'/'}
-        linkComponent={({ to, children }) => <a href={to as string}>{children}</a>}
-        actionContent={actionContent}
-      />,
-    );
-    expect(document.querySelector('svg')).not.toBeNull();
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   it('renders HeroCard with a link when size is sm', () => {
     render(
       <HeroCard
         to="/"
-        linkComponent={({ to, children }) => <a href={to as string}>{children}</a>}
+        LinkComponent={({ to, children }) => <a href={to as string}>{children}</a>}
+        buttonLabel={buttonLabel}
         title={title}
         content={content}
         backgroundColor={backgroundColor}
@@ -107,7 +52,6 @@ describe('HeroCard', () => {
       />,
     );
 
-    // Assert HeroCard is wrapped in a link
     const linkElement = screen.getByRole('link');
     expect(linkElement).toBeInTheDocument();
   });
@@ -116,7 +60,8 @@ describe('HeroCard', () => {
     render(
       <HeroCard
         to="/"
-        linkComponent={({ to, children }) => <a href={to as string}>{children}</a>}
+        LinkComponent={({ to, children }) => <a href={to as string}>{children}</a>}
+        buttonLabel={buttonLabel}
         title={title}
         content={content}
         backgroundColor={backgroundColor}
@@ -124,7 +69,6 @@ describe('HeroCard', () => {
       />,
     );
 
-    // Assert HeroCard is wrapped in a link
     const linkElement = screen.getByRole('link');
     expect(linkElement).toBeInTheDocument();
     const clickEvent = createEvent.click(linkElement);
