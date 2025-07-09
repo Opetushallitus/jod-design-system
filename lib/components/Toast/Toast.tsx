3c116580a9571cd89cd126b20e01b5e4e592610a
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2025-07-09 17:09:16 +0300

    OPHJOD-1767: Update Toast component to match latest design

--- lib/components/Toast/Toast.tsx
+++ lib/components/Toast/Toast.tsx
@@ -1,30 +1,64 @@
+import { cx } from '../../cva';
+import { JodCheckmark, JodError, JodInfo, JodWarning } from '../../icons';
+import { tidyClasses } from '../../utils';
+
 export interface ToastProps {
   /** Text shown on the toast */
   text: string;
-  /** Icon shown on the toast */
+  /** Override default toast icon */
   icon?: React.ReactNode;
   /** Variant of the toast */
-  variant?: 'success' | 'warning' | 'error';
+  variant?: 'success' | 'warning' | 'error' | 'neutral';
 }
 
 /**
  * Toasts display brief, temporary notifications. They are noticeable but do not disrupt the user experience and do not require an action to be taken.
  */
-export const Toast = ({ text, icon, variant = 'success' }: ToastProps) => (
-  <div
-    role="alert"
-    aria-live="assertive"
-    aria-atomic="true"
-    className={`
-        ${variant === 'success' ? 'ds:bg-success' : ''}
-        ${variant === 'warning' ? 'ds:bg-warning' : ''}
-        ${variant === 'error' ? 'ds:bg-alert' : ''}
-        ds:inline-flex ds:h-8 ds:items-center ds:gap-3 ds:rounded-sm ds:pl-5 ds:text-primary-gray
-        ds:text-body-sm ds:font-arial ${icon ? 'ds:pr-7' : 'ds:pr-5'}`
-      .replace(/\s+/g, ' ')
-      .trim()}
-  >
-    {icon ?? null}
-    {text}
-  </div>
-);
+export const Toast = ({ text, icon, variant = 'success' }: ToastProps) => {
+  const getDefaultIcon = () => {
+    switch (variant) {
+      case 'success':
+        return <JodCheckmark className="ds:text-success" />;
+      case 'warning':
+        return <JodWarning className="ds:text-secondary-3" />;
+      case 'error':
+        return <JodError className="ds:text-alert-text" />;
+      case 'neutral':
+        return <JodInfo className="ds:text-secondary-gray" />;
+      default:
+        return null;
+    }
+  };
+  return (
+    <div
+      role="alert"
+      aria-live="assertive"
+      aria-atomic="true"
+      className={tidyClasses([
+        cx({
+          'ds:text-secondary-gray': variant === 'neutral',
+          'ds:border-success ': variant === 'success',
+          'ds:border-warning ': variant === 'warning',
+          'ds:border-alert ': variant === 'error',
+        }),
+        variant === 'neutral' ? 'ds:bg-bg-gray' : 'ds:bg-white ds:border-2',
+        'ds:min-w-[240px]',
+        'ds:max-w-[480px]',
+        'ds:inline-flex',
+        'ds:min-h-8',
+        'ds:items-center',
+        'ds:gap-3',
+        'ds:rounded-sm',
+        'ds:py-3',
+        'ds:pl-4',
+        'ds:pr-5',
+        'ds:text-primary-gray',
+        'ds:text-body-sm',
+        'ds:font-arial',
+      ])}
+    >
+      <span className="ds:shrink-0">{icon ?? getDefaultIcon()}</span>
+      <span>{text}</span>
+    </div>
+  );
+};
