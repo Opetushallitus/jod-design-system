a816f01263bdb8bdedf3c7e49714cf63a7f44dfb
Lauri Holopainen <lauri.holopainen@elenium.fi>
2025-11-25 13:15:17 +0200

    OPHJOD-2643: Enable embedded chatbot

--- lib/components/Chatbot/Chatbot.stories.tsx
+++ lib/components/Chatbot/Chatbot.stories.tsx
@@ -19,16 +19,5 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {
-    agent: '2c134474-326f-4456-9139-8e585a569a9a',
-    language: 'fi',
-    header: 'Neuvontabotti',
-    openWindowText: 'Kysy neuvoa?',
-    agentName: 'Neuvontabotti',
-    waitingmessage: 'Haetaan tietoa...',
-    errorMessage: 'Tapahtui jokin virhe.',
-    greeting: 'Hei, olen Osaamispolun neuvontabotti.',
-    textInputPlaceholder: 'Kirjoita kysymyksesi tähän',
-    disclaimer: 'Älä syötä henkilötietojasi neuvontabotille.',
-  },
+  args: {},
 };
