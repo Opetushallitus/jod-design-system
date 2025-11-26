88f110b4fd590abc9ca981d8cce5098fe41d638c
Lauri Holopainen <lauri.holopainen@elenium.fi>
2025-11-26 09:57:40 +0200

    OPHJOD-2643: Remove unnecessary code

--- lib/components/Chatbot/Chatbot.tsx
+++ lib/components/Chatbot/Chatbot.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
 
-declare module 'react/jsx-runtime' {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace JSX {
-    interface IntrinsicElements {
-      'ai-agent-float-container': unknown;
-      'ai-agent-view-switch': unknown;
-      'ai-agent-embed': unknown;
-      'ai-agent-dialog-row': unknown;
-    }
-  }
-}
-
 /** A component for displaying the AI chatbot */
 export const Chatbot = () => {
   const src = 'https://okm-ps32.aiagent.fi/chat/AIAgentChatComponent.js';
   React.useEffect(() => {
     const script = document.createElement('script');
-    script.src = 'https://okm-ps32.aiagent.fi/chat/AIAgentChatComponent.js';
+    script.src = src;
     script.async = true;
     document.body.appendChild(script);
 
