b4550c2a17c128de8bac1faf5626f16fe816ad66
Lauri Holopainen <lauri.holopainen@elenium.fi>
2025-11-28 12:53:31 +0200

    OPHJOD-2790: Add new icons, JodFlagFilled and JodRoute

---
+++ lib/icons/JodFlagFilled.tsx
@@ -0,0 +1,10 @@
+import IconWrapper from './SvgWrapper';
+import { IconProps } from './utils';
+
+export const JodFlagFilled = (props: IconProps) => {
+  return (
+    <IconWrapper {...props}>
+      <path d="M0 17V0H9L9.4 2H15V12H8L7.6 10H2V17H0Z" fill="currentColor" />
+    </IconWrapper>
+  );
+};
