9eba522ab900973e2767d11637576b99a68080da
Lauri Holopainen <lauri.holopainen@elenium.fi>
2025-12-01 12:38:32 +0200

    OPHJOD-2643: Hide chatbot in prd

--- lib/components/Chatbot/Chatbot.tsx
+++ lib/components/Chatbot/Chatbot.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import { useEnvironment } from '../../hooks/useEnvironment';
 
 /** A component for displaying the AI chatbot */
 export const Chatbot = () => {
+  const { isProd } = useEnvironment();
   const src = 'https://okm-ps32.aiagent.fi/chat/AIAgentChatComponent.js';
   React.useEffect(() => {
+    if (isProd) return;
     const script = document.createElement('script');
     script.src = src;
     script.async = true;
     document.body.appendChild(script);
-
     return () => {
       document.body.removeChild(script);
     };
-  }, [src]);
+  }, [src, isProd]);
 
   return null;
 };
