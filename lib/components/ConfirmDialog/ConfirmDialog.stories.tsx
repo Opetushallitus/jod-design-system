0dbb6ec12ef1ce24765d8a94b2a3010eeac0d187
Sami Korpela <sami.korpela@gofore.com>
2026-02-04 15:12:29 +0200

    OPHJOD-3034: Update ConfirmDialog design

--- lib/components/ConfirmDialog/ConfirmDialog.stories.tsx
+++ lib/components/ConfirmDialog/ConfirmDialog.stories.tsx
@@ -89,7 +89,7 @@ export const Destructive: Story = {
 const Button = ({ label, onClick }: { label: string; onClick?: () => void }) => {
   return (
     <button
-      className="ds:cursor-pointer ds:group ds:flex ds:select-none ds:items-center ds:gap-4 ds:rounded-[30px] ds:bg-white ds:px-6 ds:text-button-md ds:text-primary-gray ds:hover:text-accent ds:focus-visible:text-accent ds:focus-visible:outline ds:focus-visible:outline-[3px] ds:focus-visible:outline-offset-[1.5px] ds:focus-visible:outline-accent ds:active:bg-accent ds:active:text-white ds:active:outline-0"
+      className="ds:cursor-pointer ds:group ds:flex ds:select-none ds:items-center ds:gap-4 ds:rounded-[30px] ds:bg-white ds:px-6 ds:text-button-md ds:text-primary-gray ds:hover:text-accent ds:focus-visible:text-accent ds:focus-visible:outline-[3px] ds:focus-visible:outline-offset-[1.5px] ds:focus-visible:outline-accent ds:active:bg-accent ds:active:text-white ds:active:outline-0"
       onClick={onClick}
     >
       <span className="ds:py-[10px] ds:group-hover:underline ds:group-focus-visible:no-underline ds:group-active:no-underline">
@@ -106,7 +106,7 @@ const renderFooter = (args: Story['args']) => {
     <ConfirmDialog
       {...rest}
       footer={(hideDialog) => (
-        <div className="ds:flex ds:flex-row ds:justify-between ds:gap-5">
+        <div className="ds:flex ds:flex-shrink-0 ds:bg-bg-gray-2 ds:overflow-x-auto ds:overflow-y-hidden ds:justify-between ds:z-50 ds:gap-3">
           <Button onClick={hideDialog} label="En" />
           <Button
             onClick={() => {
@@ -135,6 +135,6 @@ export const WithFooter: Story = {
     footer: () => <></>,
     title: 'Haluatko kokeilla uutta ominaisuutta?',
     description:
-      'Lorem ipsum dolor sit amet, no vis verear commodo. Vix quot dicta phaedrum ad. Has eu invenire concludaturque, simul accusata no ius.',
+      "I'm baby vice cupping salvia hella activated charcoal yuccie selvage twee la croix. PBR&B bruh gentrify, sartorial forage viral succulents craft beer trust fund cornhole. Chartreuse tonx tumeric, pour-over messenger bag next level gluten-free sus cronut gochujang gastropub hashtag. Crucifix vibecession man bun mlkshk vape. Cray mumblecore neutra ascot, bodega boys blog PBR&B tumblr woke kogi venmo. Bespoke pour-over chambray echo park meditation crucifix PBR&B yr 8-bit sustainable fanny pack green juice cray stumptown. Schlitz tofu celiac try-hard palo santo pickled big mood. Cliche tote bag food truck, affogato godard roof party next level succulents. Dreamcatcher roof party distillery literally vinyl succulents XOXO microdosing. PBR&B fanny pack hexagon vexillologist solarpunk. Brunch raw denim vexillologist meggings, kale chips sriracha man bun chillwave. Plaid vexillologist fanny pack, kitsch chambray same next level mukbang PBR&B meditation. La croix deep v skateboard tumblr fixie shoreditch. Mustache humblebrag put a bird on it beard forage, paleo health goth brunch DSA dreamcatcher vinyl unicorn. Tumeric jawn hell of iceland flexitarian chartreuse lumbersexual butcher adaptogen heirloom offal humblebrag. Franzen sartorial enamel pin, butcher tattooed irony food truck cold-pressed tousled chia narwhal tilde vaporware tumeric. Subway tile scenester coloring book, flannel portland bushwick readymade hoodie.",
   },
 };
