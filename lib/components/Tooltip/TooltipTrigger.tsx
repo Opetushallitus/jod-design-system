d6bf0f3e2bb740b3a1dc42cc51a9f3d0a5966790
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2026-03-25 09:13:08 +0200

    OPHJOD-2971: Fix a11y issues with Tag component tooltips

--- lib/components/Tooltip/TooltipTrigger.tsx
+++ lib/components/Tooltip/TooltipTrigger.tsx
@@ -11,9 +11,13 @@ export const TooltipTrigger = React.forwardRef<
     ariaLabel?: string;
     open?: boolean;
     toggleOnClick?: boolean;
+    noAriaDescribedby?: boolean;
     onClick?: React.MouseEventHandler<HTMLElement>;
   }
->(function TooltipTrigger({ children, open, asChild = false, ariaLabel, toggleOnClick = false, ...props }, propRef) {
+>(function TooltipTrigger(
+  { children, open, asChild = false, ariaLabel, toggleOnClick = false, noAriaDescribedby = false, ...props },
+  propRef,
+) {
   const context = useTooltipContext();
   const childrenRef = (children as { ref: React.ForwardedRef<HTMLElement> }).ref;
   const ref = useMergeRefs([context.refs.setReference, propRef, childrenRef]);
@@ -30,6 +34,14 @@ export const TooltipTrigger = React.forwardRef<
         context.setOpen(!context.open);
       }
     : onClick;
+
+  // According to testers, because our tooltips are most often triggered by hover or focus and
+  // not via click or button press, and because the tooltip does not exist hidden in the DOM,
+  // the "aria-describedby" (which is automatically added by floating-ui/react) should not be  added.
+  const defaultAriaDescribedby = context.getReferenceProps()[
+    'aria-describedby'
+  ] as React.HTMLProps<HTMLElement>['aria-describedby'];
+  const ariaDescribedby = noAriaDescribedby ? undefined : defaultAriaDescribedby;
 
   if (asChild && React.isValidElement(children)) {
     // Extract any onClick already on the child
@@ -57,8 +69,8 @@ export const TooltipTrigger = React.forwardRef<
       ref,
       onClick: finalOnClick,
       ...(testId ? { 'data-testid': testId } : {}),
-      'aria-expanded': context.open,
       'aria-label': ariaLabel,
+      'aria-describedby': ariaDescribedby,
     } as React.HTMLProps<HTMLElement> & { 'data-testid'?: string };
 
     // eslint-disable-next-line react-hooks/refs
@@ -70,9 +82,9 @@ export const TooltipTrigger = React.forwardRef<
       aria-label={ariaLabel}
       ref={ref}
       {...context.getReferenceProps({ ...rest, onClick: baseToggleHandler })}
+      aria-describedby={ariaDescribedby}
       data-testid={testId}
       className="ds:cursor-pointer"
-      aria-expanded={context.open}
       type="button"
     >
       {children}
