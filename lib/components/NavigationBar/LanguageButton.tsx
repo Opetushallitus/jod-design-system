2ec9047e1625782418e640d2f671aeade9e612f0
Sauli Anto <sauli.anto@gofore.com>
2026-04-20 12:35:22 +0300

    OPHJOD-3337: Fix Cookie Consent for improved layout

--- lib/components/NavigationBar/LanguageButton.tsx
+++ lib/components/NavigationBar/LanguageButton.tsx
@@ -2,6 +2,7 @@ import React, { JSX } from 'react';
 import { useMediaQueries } from '../../hooks/useMediaQueries';
 import { usePopupMenu } from '../../hooks/usePopupMenu';
 import { JodCaretDown, JodCaretUp, JodLanguage } from '../../icons';
+import { cx } from '../../main';
 import { LanguageMenu } from './LanguageMenu';
 import { PopupMenuWrapper } from './PopupMenuWrapper';
 import { LangCode, LanguageButtonProps, LanguageTranslations } from './types';
@@ -31,22 +32,31 @@ export const LanguageButton = ({
   supportedLanguageCodes,
   generateLocalizedPath,
   linkComponent: LinkComponent,
+  responsive = true,
   translations,
   testId,
 }: LanguageButtonProps) => {
   const { md } = useMediaQueries();
   const { isOpen: langMenuOpen, close: closeLanguageMenu, triggerProps, menuProps } = usePopupMenu();
-  const carets = md ? <>{langMenuOpen ? <JodCaretUp size={20} /> : <JodCaretDown size={20} />}</> : null;
+  const carets = md || !responsive ? <>{langMenuOpen ? <JodCaretUp size={20} /> : <JodCaretDown size={20} />}</> : null;
 
   return (
     <div className="ds:relative" data-testid={testId}>
       <button
         {...triggerProps}
-        className="ds:flex ds:flex-col ds:md:flex-row ds:justify-center ds:items-center ds:select-none ds:cursor-pointer ds:gap-y-2 ds:gap-x-3 ds:text-primary-gray"
+        className={cx(
+          'ds:flex ds:flex-col ds:justify-center ds:items-center ds:select-none ds:cursor-pointer ds:gap-y-2 ds:gap-x-3 ds:text-primary-gray',
+          (md || !responsive) && 'ds:flex-row',
+        )}
         data-testid={testId ? `${testId}-trigger` : undefined}
       >
         <JodLanguage className="mx-auto" />
-        <span className="ds:whitespace-nowrap ds:md:text-[14px] ds:sm:text-[12px] ds:text-[10px] ds:font-semibold">
+        <span
+          className={cx(
+            'ds:whitespace-nowrap ds:font-semibold',
+            responsive ? 'ds:md:text-[14px] ds:sm:text-[12px] ds:text-[10px]' : 'ds:text-[14px]',
+          )}
+        >
           {translations[language].label}
           {getLanguageOrder(language, translations)}
         </span>
