aaee8ebeb97540a786dec3b930e2cce85be14ac3
Jussi Sirén <jussi.siren@sleek.works>
2026-05-08 12:52:12 +0300

    OPHJOD-2512: Navigationmenu improvements

--- lib/components/NavigationMenu/components/LanguageSelection.tsx
+++ lib/components/NavigationMenu/components/LanguageSelection.tsx
@@ -1,9 +1,9 @@
+import { cx } from '../../../cva';
 import { useServiceVariant } from '../../../hooks/useServiceVariant/useServiceVariant';
 import {
   getAccentBgClassForService,
   getFocusOutlineClassForService,
-  getPressedBgColorClassForService,
-  tidyClasses,
+  getGroupActiveBgColorClassForService,
 } from '../../../utils';
 import { LinkComponent } from '../types';
 
@@ -29,31 +29,48 @@ export const LanguageSelection = ({ items, selected, title }: NavigationMenuLang
     <>
       <h2 className="ds:text-body-sm ds:mb-5 ds:mt-2 ds:flex">{title}</h2>
       <div className="ds:flex">
-        <ul className="ds:flex ds:flex-1 ds:flex-col">
+        <ul className="ds:flex ds:flex-1 ds:flex-col ds:gap-2">
           {items.map((item: LanguageSelectionItem) => (
-            <li key={item.label} className="ds:flex ds:min-h-8 ds:items-center ds:hover:underline" lang={item.value}>
+            <li key={item.label} className="ds:flex ds:min-h-8 ds:items-center" lang={item.value}>
               <item.linkComponent
-                className={tidyClasses([
+                className={cx([
                   'ds:flex',
                   'ds:flex-row',
                   'ds:flex-1',
                   'ds:items-center',
-                  'ds:hover:bg-bg-gray',
-                  'ds:rounded',
-                  'ds:text-primary-gray',
-                  'ds:active:text-white',
-                  'ds:py-3',
-                  getPressedBgColorClassForService(serviceVariant),
+                  'ds:text-button-md',
+                  'ds:group',
                   getFocusOutlineClassForService(serviceVariant),
                 ])}
                 aria-current={selected === item.value}
               >
-                <span className="ds:size-4 ds:flex ds:justify-center ds:items-center" aria-hidden>
-                  {selected === item.value && (
-                    <span className={`${getAccentBgClassForService(serviceVariant)} ds:size-4 ds:rounded-full`} />
-                  )}
+                <span className="ds:size-4 ds:flex ds:justify-center ds:items-center ds:ml-2" aria-hidden>
+                  <span
+                    className={cx([
+                      selected === item.value && getAccentBgClassForService(serviceVariant),
+                      'ds:size-4',
+                      'ds:rounded-full',
+                      selected !== item.value && 'ds:group-hover:bg-secondary-5-light-3',
+                      getGroupActiveBgColorClassForService(serviceVariant),
+                    ])}
+                  />
                 </span>
-                <span className="ds:text-button-md ds:ml-3">{item.label}</span>
+                <span
+                  className={cx([
+                    'ds:ml-2',
+                    'ds:rounded',
+                    'ds:text-primary-gray',
+                    selected !== item.value && 'ds:group-hover:bg-secondary-5-light-3',
+                    'ds:group-hover:underline',
+                    'ds:py-3',
+                    'ds:px-3',
+                    'ds:grow',
+                    'ds:group-active:text-white',
+                    getGroupActiveBgColorClassForService(serviceVariant),
+                  ])}
+                >
+                  {item.label}
+                </span>
               </item.linkComponent>
             </li>
           ))}
