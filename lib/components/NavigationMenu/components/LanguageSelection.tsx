946420c807d94832ab3fdf0b592632d8b1b3959c
Sami Korpela <sami.korpela@gofore.com>
2026-09-14 10:52:00 +0300

    OPHJOD-3560: Language selection hover style and refactor

--- lib/components/NavigationMenu/components/LanguageSelection.tsx
+++ lib/components/NavigationMenu/components/LanguageSelection.tsx
@@ -1,10 +1,5 @@
-import { cx } from '../../../cva';
 import { useServiceVariant } from '../../../hooks/useServiceVariant/useServiceVariant';
-import {
-  getAccentBgClassForService,
-  getFocusOutlineClassForService,
-  getGroupActiveBgColorClassForService,
-} from '../../../utils';
+import { LanguageSelectionItem as LanguageSelectionListItem } from '../../internal/LanguageSelectionItem/LanguageSelectionItem';
 import { LinkComponent } from '../types';
 
 export interface LanguageSelectionItem {
@@ -32,56 +27,21 @@ export const LanguageSelection = ({ items, selected, title }: NavigationMenuLang
       </h2>
       <div className="ds:flex">
         <ul className="ds:flex ds:flex-1 ds:flex-col ds:gap-2" data-testid="language-selection-list">
-          {items.map((item: LanguageSelectionItem) => (
-            <li
-              key={item.label}
-              className="ds:flex ds:min-h-8 ds:items-center"
-              lang={item.value}
-              data-testid={`language-selection-item-${item.label.replace(/\s+/g, '-').toLowerCase()}`}
-            >
-              <item.linkComponent
-                className={cx([
-                  'ds:flex',
-                  'ds:flex-row',
-                  'ds:flex-1',
-                  'ds:items-center',
-                  'ds:text-button-md',
-                  'ds:group',
-                  getFocusOutlineClassForService(serviceVariant),
-                ])}
-                aria-current={selected === item.value}
-                data-testid={`language-selection-item-link-${item.label.replace(/\s+/g, '-').toLowerCase()}`}
-              >
-                <span className="ds:size-4 ds:flex ds:justify-center ds:items-center ds:ml-2" aria-hidden>
-                  <span
-                    className={cx([
-                      selected === item.value && getAccentBgClassForService(serviceVariant),
-                      'ds:size-4',
-                      'ds:rounded-full',
-                      selected !== item.value && 'ds:group-hover:bg-primary-5-light-3',
-                      getGroupActiveBgColorClassForService(serviceVariant),
-                    ])}
-                  />
-                </span>
-                <span
-                  className={cx([
-                    'ds:ml-2',
-                    'ds:rounded',
-                    'ds:text-primary-gray',
-                    selected !== item.value && 'ds:group-hover:bg-primary-5-light-3',
-                    'ds:group-hover:underline',
-                    'ds:py-3',
-                    'ds:px-3',
-                    'ds:grow',
-                    'ds:group-active:text-white',
-                    getGroupActiveBgColorClassForService(serviceVariant),
-                  ])}
-                >
-                  {item.label}
-                </span>
-              </item.linkComponent>
-            </li>
-          ))}
+          {items.map((item: LanguageSelectionItem) => {
+            const testId = `language-selection-item-${item.label.replace(/\s+/g, '-').toLowerCase()}`;
+
+            return (
+              <LanguageSelectionListItem
+                key={item.label}
+                label={item.label}
+                value={item.value}
+                selected={selected === item.value}
+                serviceVariant={serviceVariant}
+                linkComponent={item.linkComponent}
+                testId={testId}
+              />
+            );
+          })}
         </ul>
       </div>
     </>
